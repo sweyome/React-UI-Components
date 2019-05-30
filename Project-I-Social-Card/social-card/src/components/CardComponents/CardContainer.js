1acf6fdaf2f580829cc5c042d827a330f98f1419
sweyome <sweyome1@gmail.com>
2019-05-29 21:56:52 -0400

    proj pushed on my branch

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+const CardContainer = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return ( 
+        <div>
+
+            <p>This is a header Title</p>
+        </div>
+     );
+}
+ 
+export default HeaderTitle;
